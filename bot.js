fcc368d549549494a40f0a730cfb3138bdad66b6
AlexPrice17 <73586223+AlexPrice17@users.noreply.github.com>
2021-02-09 13:37:44 +0000

    Added chocolate, cake and crisps to the programme

--- bot.js
+++ bot.js
@@ -6,8 +6,11 @@ function saySomething() {
   if (message.indexOf("rain") != -1 || message.indexOf("sun") != -1 || message.indexOf("weather") != -1)
     answer = "I do not care too much about weather, I'm locked inside a data center.";
 
-  else if (message.indexOf("tea") != -1 || message.indexOf("buscuit") != -1 || message.indexOf("buscuits") != -1)
-    answer = "I like tea and buscuits!"
+  else if (message.indexOf("tea") != -1 || message.indexOf("biscuit") != -1 || message.indexOf("biscuits") != -1)
+    answer = "I like tea and biscuits!"
+  
+    else if (message.indexOf("chocolate") != -1 || message.indexOf("cake") != -1 || message.indexOf("crisps") != -1)
+      answer = "I like chocolate, cake and crisps!"
 
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
   conversation.innerHTML += "<p><strong>Useless bot:</strong> " + answer + "</p>";
